b081b43b7d4eb882f079c1c80e45d26c47123886
Kevin Mees <kev.mees@gmail.com>
2014-02-18 15:52:18 +0100

    Added ngdoc comments to $spPageContextInfo

--- src/services/$spPageContextInfo.js
+++ src/services/$spPageContextInfo.js
@@ -1,13 +1,23 @@
+/**
+ * @ngdoc object
+ * @name ExpertsInside.SharePoint.$spPageContextInfo
+ * @requires $window
+ *
+ * @description
+ * Wraps the global '_spPageContextInfo' object in an angular service
+ *
+ * @return {object} $spPageContextInfo Copy of the global '_spPageContextInfo' object
+ */
 angular.module('ExpertsInside.SharePoint')
   .factory('$spPageContextInfo', function($rootScope, $window) {
     'use strict';
 
-    var me = { };
-    angular.copy($window._spPageContextInfo, me);
+    var $spPageContextInfo = { };
+    angular.copy($window._spPageContextInfo, $spPageContextInfo);
 
     $rootScope.$watch(function() { return $window._spPageContextInfo; }, function(spPageContextInfo) {
-      angular.copy(spPageContextInfo, me);
+      angular.copy(spPageContextInfo, $spPageContextInfo);
     }, true);
 
-    return me;
+    return $spPageContextInfo;
   });
