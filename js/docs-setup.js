f88257271cf38663cea38928365f0f0d3ad0b32b
Kevin Mees <kev.mees@gmail.com>
2014-02-19 11:14:45 +0100

    Initial commit

---
+++ js/docs-setup.js
@@ -0,0 +1,42 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint",
+      "shortName": "ExpertsInside.SharePoint",
+      "type": "overview",
+      "moduleName": "ExpertsInside.SharePoint",
+      "shortDescription": "The main module which holds everything together.",
+      "keywords": "api expertsinside holds main module overview sharepoint"
+    },
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.$spList",
+      "shortName": "ExpertsInside.SharePoint.$spList",
+      "type": "service",
+      "moduleName": "ExpertsInside.SharePoint",
+      "shortDescription": "A factory which creates a list object that lets you interact with SharePoint Lists via the",
+      "keywords": "$http $splist $sppagecontextinfo action actions api behaviors class creates default expertsinside factory high-level interact level list lists low methods ng object provide resource rest returned service set sharepoint"
+    },
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.$spPageContextInfo",
+      "shortName": "ExpertsInside.SharePoint.$spPageContextInfo",
+      "type": "object",
+      "moduleName": "ExpertsInside.SharePoint",
+      "shortDescription": "Wraps the global &#39;_spPageContextInfo&#39; object in an angular service",
+      "keywords": "$rootscope $sppagecontextinfo $window _sppagecontextinfo angular api copy expertsinside global object service sharepoint wraps"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
