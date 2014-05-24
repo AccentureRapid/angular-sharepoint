90e3b9d61a3fa939c034cc3884c6d90ce405510c
Kevin Mees <kev.mees@gmail.com>
2014-05-24 11:01:46 +0200

    Better ShareCoffee detection

--- src/sharepoint.js
+++ src/sharepoint.js
@@ -7,14 +7,9 @@
  * The main module which holds everything together.
  */
 angular.module('ExpertsInside.SharePoint', ['ng'])
-  .run(function() {
-    var sharepointMinErr = angular.$$minErr('sharepoint');
-
-    if (angular.isUndefined(ShareCoffee)) {
-      throw sharepointMinErr(
-        "noShareCoffee", "angular-sharepoint depends on ShareCoffee to do its job." +
-        "Either include the bundled ShareCoffee + angular-sharepoint file " +
-        "or include ShareCoffe seperately before angular-sharepoint."
-      );
+  .run(function($window) {
+    if (angular.isUndefined($window.ShareCoffee)) {
+      console.error("angular-sharepoint requires ShareCoffee to do its job. " +
+                    "Please include ShareCoffe.js in your document");
     }
   });
