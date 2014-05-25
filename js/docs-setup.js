44364aeef7ecf730e5018a18f6e88c5a3a7240ac
Kevin Mees <kev.mees@gmail.com>
2014-05-25 14:44:18 +0200

    Updated docs

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -9,8 +9,8 @@ NG_DOCS={
       "shortName": "ExpertsInside.SharePoint",
       "type": "overview",
       "moduleName": "ExpertsInside.SharePoint",
-      "shortDescription": "The main module which holds everything together.",
-      "keywords": "api expertsinside holds main module overview sharepoint"
+      "shortDescription": "ExpertsInside.SharePoint",
+      "keywords": "$ngresource $splist abstraction api expertsinside high interaction level lists module overview rest sharepoint similiar usage"
     },
     {
       "section": "api",
@@ -18,8 +18,8 @@ NG_DOCS={
       "shortName": "ExpertsInside.SharePoint.$spList",
       "type": "service",
       "moduleName": "ExpertsInside.SharePoint",
-      "shortDescription": "A factory which creates a list object that lets you interact with SharePoint Lists via the",
-      "keywords": "$http $splist $sppagecontextinfo action actions api behaviors class creates default expertsinside factory high-level interact level list lists low methods ng object provide resource rest returned service set sharepoint"
+      "shortDescription": "A factory which creates a list item resource object that lets you interact with",
+      "keywords": "$expand $filter $http $orderby $select $skip $sort $splist $sprest $top accept accepting action actions addnamedquery api array based behaviors bytitle call caution changes check class completed concurrent create creates custom default defined defining delete deletes details empty eq error etag excluded existing exlcuded expertsinside factory field filter foo footodo force function hash high-level instance interact item items js level list low methods names object optional options overridden overwrites parameter parameters passed place prefixing properties provide queries query querydefaults readonlyfields request resource rest retrieve returned returns save saving server service set sharepoint single singleresult string supported supports throws title todo todos true uncompleted uncompletedtodos update updates var"
     },
     {
       "section": "api",
@@ -29,12 +29,30 @@ NG_DOCS={
       "moduleName": "ExpertsInside.SharePoint",
       "shortDescription": "Wraps the global &#39;_spPageContextInfo&#39; object in an angular service",
       "keywords": "$rootscope $sppagecontextinfo $window _sppagecontextinfo angular api copy expertsinside global object service sharepoint wraps"
+    },
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.$spSearch",
+      "shortName": "ExpertsInside.SharePoint.$spSearch",
+      "type": "service",
+      "moduleName": "ExpertsInside.SharePoint",
+      "shortDescription": "Query the Search via REST API",
+      "keywords": "$spconvert $sprest $spsearch api expertsinside query rest search service sharepoint"
+    },
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.$spUser",
+      "shortName": "ExpertsInside.SharePoint.$spUser",
+      "type": "service",
+      "moduleName": "ExpertsInside.SharePoint",
+      "shortDescription": "Load user information via UserProfiles REST API",
+      "keywords": "$spconvert $sprest $spuser api expertsinside load rest service sharepoint user userprofiles"
     }
   ],
   "apis": {
     "api": true
   },
-  "html5Mode": true,
+  "html5Mode": false,
   "startPage": "/api",
   "scripts": [
     "angular.min.js"
