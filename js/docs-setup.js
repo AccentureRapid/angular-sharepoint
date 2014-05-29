8c5ffbee49b7f06feafb2b3dc4b23be98919caae
Kevin Mees <kev.mees@gmail.com>
2014-05-30 01:54:18 +0200

    Updated docs

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -14,24 +14,6 @@ NG_DOCS={
     },
     {
       "section": "api",
-      "id": "ExpertsInside.SharePoint.$spList",
-      "shortName": "ExpertsInside.SharePoint.$spList",
-      "type": "service",
-      "moduleName": "ExpertsInside.SharePoint",
-      "shortDescription": "A factory which creates a list item resource object that lets you interact with",
-      "keywords": "$expand $filter $http $orderby $select $skip $sort $splist $sprest $top accept accepting action actions addnamedquery api array based behaviors bytitle call caution changes check class completed concurrent create creates custom default defined defining delete deletes details empty eq error etag excluded existing exlcuded expertsinside factory field filter foo footodo force function hash high-level instance interact item items js level list low methods names object optional options overridden overwrites parameter parameters passed place prefixing properties provide queries query querydefaults readonlyfields request resource rest retrieve returned returns save saving server service set sharepoint single singleresult string supported supports throws title todo todos true uncompleted uncompletedtodos update updates var"
-    },
-    {
-      "section": "api",
-      "id": "ExpertsInside.SharePoint.$spPageContextInfo",
-      "shortName": "ExpertsInside.SharePoint.$spPageContextInfo",
-      "type": "object",
-      "moduleName": "ExpertsInside.SharePoint",
-      "shortDescription": "Wraps the global &#39;_spPageContextInfo&#39; object in an angular service",
-      "keywords": "$rootscope $sppagecontextinfo $window _sppagecontextinfo angular api copy expertsinside global object service sharepoint wraps"
-    },
-    {
-      "section": "api",
       "id": "ExpertsInside.SharePoint.$spSearch",
       "shortName": "ExpertsInside.SharePoint.$spSearch",
       "type": "service",
@@ -47,6 +29,33 @@ NG_DOCS={
       "moduleName": "ExpertsInside.SharePoint",
       "shortDescription": "Load user information via UserProfiles REST API",
       "keywords": "$spconvert $sprest $spuser api expertsinside load rest service sharepoint user userprofiles"
+    },
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.Core.$spPageContextInfo",
+      "shortName": "ExpertsInside.SharePoint.Core.$spPageContextInfo",
+      "type": "object",
+      "moduleName": "ExpertsInside.SharePoint.Core",
+      "shortDescription": "A reference to the documents _spPageContextInfo object. While _spPageContextInfo",
+      "keywords": "$rootscope $scope $sppagecontextinfo $window _sppagecontextinfo api core ctrl documents expertsinside function global globally javascript js mocked object overridden problems reference referring removed service sharepoint testability testing thorugh userloginname username variable"
+    },
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.Core.$spRest",
+      "shortName": "ExpertsInside.SharePoint.Core.$spRest",
+      "type": "service",
+      "moduleName": "ExpertsInside.SharePoint.Core",
+      "shortDescription": "Utility functions when interacting with the SharePoint REST API",
+      "keywords": "$log $select $sprest accepts allowed api app appendqueryparameters appends bar baz body buildquerystring builds core create expertsinside foo function functions http interacting invalid js json missing normalized normalizeparams normalizes parameters params parse prefixing qs query remove removing response rest select service sharepoint string title transformed transformresponse url utility var whitelist wrapper"
+    },
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.List.$spList",
+      "shortName": "ExpertsInside.SharePoint.List.$spList",
+      "type": "service",
+      "moduleName": "ExpertsInside.SharePoint.List",
+      "shortDescription": "A factory which creates a list item resource object that lets you interact with",
+      "keywords": "$expand $filter $http $orderby $select $skip $sort $spconvert $splist $sprest $top accept accepting action actions addnamedquery api array based behaviors bytitle call caution changes check class completed concurrent core create creates custom default defined defining delete deletes details empty eq error etag excluded existing exlcuded expertsinside factory field filter foo footodo force function hash high-level instance interact item items js level list listoptions low methods names object optional options overridden overwrites parameter parameters passed place prefixing properties provide queries query readonlyfields request resource rest retrieve returned returns save saving server service set sharepoint single singleresult string supported supports throws title todo todos true uncompleted uncompletedtodos update updates var"
     }
   ],
   "apis": {
