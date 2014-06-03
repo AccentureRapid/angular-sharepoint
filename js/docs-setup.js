1846982906eb3aa00f49b4ba9bfec9af46c14eae
Kevin Mees <kev.mees@gmail.com>
2014-06-03 13:54:08 +0200

    Updated docs

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -6,30 +6,39 @@ NG_DOCS={
   "pages": [
     {
       "section": "api",
+      "id": "index",
+      "shortName": "angular-sharepoint API",
+      "type": "overview",
+      "moduleName": "angular-sharepoint API",
+      "shortDescription": "angular-sharepoint API",
+      "keywords": "angular-sharepoint api overview"
+    },
+    {
+      "section": "api",
       "id": "ExpertsInside.SharePoint",
       "shortName": "ExpertsInside.SharePoint",
       "type": "overview",
       "moduleName": "ExpertsInside.SharePoint",
       "shortDescription": "ExpertsInside.SharePoint",
-      "keywords": "$ngresource $splist abstraction api expertsinside high interaction level lists module overview rest sharepoint similiar usage"
+      "keywords": "angular-sharepoint api complete core experience expertsinside list overview search sharepoint user"
     },
     {
       "section": "api",
-      "id": "ExpertsInside.SharePoint.$spSearch",
-      "shortName": "ExpertsInside.SharePoint.$spSearch",
-      "type": "service",
-      "moduleName": "ExpertsInside.SharePoint",
-      "shortDescription": "Query the Search via REST API",
-      "keywords": "$spconvert $sprest $spsearch api expertsinside query rest search service sharepoint"
+      "id": "ExpertsInside.SharePoint.Core",
+      "shortName": "ExpertsInside.SharePoint.Core",
+      "type": "overview",
+      "moduleName": "ExpertsInside.SharePoint.Core",
+      "shortDescription": "ExpertsInside.SharePoint.Core",
+      "keywords": "api core expertsinside module modules overview services sharepoint utility"
     },
     {
       "section": "api",
-      "id": "ExpertsInside.SharePoint.$spUser",
-      "shortName": "ExpertsInside.SharePoint.$spUser",
-      "type": "service",
-      "moduleName": "ExpertsInside.SharePoint",
-      "shortDescription": "Load user information via UserProfiles REST API",
-      "keywords": "$spconvert $sprest $spuser api expertsinside load rest service sharepoint user userprofiles"
+      "id": "ExpertsInside.SharePoint.Core.$spConvert",
+      "shortName": "ExpertsInside.SharePoint.Core.$spConvert",
+      "type": "object",
+      "moduleName": "ExpertsInside.SharePoint.Core",
+      "shortDescription": "The $spConvert service exposes functions",
+      "keywords": "$spconvert api convert core datatypes edm expertsinside exposes functions javascript object objects search service sharepoint values"
     },
     {
       "section": "api",
@@ -38,7 +47,7 @@ NG_DOCS={
       "type": "object",
       "moduleName": "ExpertsInside.SharePoint.Core",
       "shortDescription": "A reference to the documents _spPageContextInfo object. While _spPageContextInfo",
-      "keywords": "$rootscope $scope $sppagecontextinfo $window _sppagecontextinfo api core ctrl documents expertsinside function global globally javascript js mocked object overridden problems reference referring removed service sharepoint testability testing thorugh userloginname username variable"
+      "keywords": "$scope $sppagecontextinfo _sppagecontextinfo api core ctrl documents expertsinside function global globally javascript js mocked object overridden problems reference referring removed service sharepoint testability testing thorugh userloginname username variable"
     },
     {
       "section": "api",
@@ -47,7 +56,16 @@ NG_DOCS={
       "type": "service",
       "moduleName": "ExpertsInside.SharePoint.Core",
       "shortDescription": "Utility functions when interacting with the SharePoint REST API",
-      "keywords": "$log $select $sprest accepts allowed api app appendqueryparameters appends bar baz body buildquerystring builds core create expertsinside foo function functions http interacting invalid js json missing normalized normalizeparams normalizes parameters params parse prefixing qs query remove removing response rest select service sharepoint string title transformed transformresponse url utility var whitelist wrapper"
+      "keywords": "$select $sprest accepts allowed api app appendqueryparameters appends bar baz body buildquerystring builds core create expertsinside foo function functions http interacting invalid js json missing normalized normalizeparams normalizes parameters params parse prefixing qs query remove removing response rest select service sharepoint string title transformed transformresponse url utility var whitelist wrapper"
+    },
+    {
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.List",
+      "shortName": "ExpertsInside.SharePoint.List",
+      "type": "overview",
+      "moduleName": "ExpertsInside.SharePoint.List",
+      "shortDescription": "ExpertsInside.SharePoint.List",
+      "keywords": "$splist api core expertsinside list module overview rest service sharepoint wrapper"
     },
     {
       "section": "api",
@@ -59,49 +77,40 @@ NG_DOCS={
       "keywords": "$expand $filter $http $orderby $select $skip $sort $spconvert $splist $sprest $top accept accepting action actions addnamedquery api array based behaviors bytitle call caution changes check class completed concurrent core create creates custom default defined defining delete deletes details empty eq error etag excluded existing exlcuded expertsinside factory field filter foo footodo force function hash high-level instance interact item items js level list listoptions low methods names object optional options overridden overwrites parameter parameters passed place prefixing properties provide queries query readonlyfields request resource rest retrieve returned returns save saving server service set sharepoint single singleresult string supported supports throws title todo todos true uncompleted uncompletedtodos update updates var"
     },
     {
-      "section": "guide",
-      "id": "index",
-      "shortName": "Guide",
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.Search",
+      "shortName": "ExpertsInside.SharePoint.Search",
       "type": "overview",
-      "moduleName": "Guide",
-      "shortDescription": "angular-sharepoint Guide",
-      "keywords": "angular-sharepoint guide overview"
+      "moduleName": "ExpertsInside.SharePoint.Search",
+      "shortDescription": "ExpertsInside.SharePoint.Search",
+      "keywords": "$spsearch api core expertsinside include js module overview rest search service sharepoint wrapper"
     },
     {
-      "section": "guide",
-      "id": "00_installation",
-      "shortName": "Installation",
-      "type": "overview",
-      "moduleName": "Installation",
-      "shortDescription": "Installation",
-      "keywords": "angular-sharepoint automatically bower build checkout clone command compile compiled components configured console dependencies directly files folder git github guide inside install install-package installation installed installing instructions io machine manager note nuget org overview package project releases repository running scripts search simply source sources specific stands studio tagged test tools version visual vx watch ways"
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.Search.$spSearch",
+      "shortName": "ExpertsInside.SharePoint.Search.$spSearch",
+      "type": "service",
+      "moduleName": "ExpertsInside.SharePoint.Search",
+      "shortDescription": "Query the Search via REST API",
+      "keywords": "$spconvert $sprest $spsearch api core expertsinside query rest search service sharepoint"
     },
     {
-      "section": "guide",
-      "id": "01_getting-started",
-      "shortName": "Getting Started",
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.User",
+      "shortName": "ExpertsInside.SharePoint.User",
       "type": "overview",
-      "moduleName": "Getting Started",
-      "shortDescription": "Getting Started",
-      "keywords": "$splist $sppagecontextinfo $sprest $spsearch $spuser additional angular angular-sharepoint angularjs api app charset complete console containg core currently declaration declare declaring dependency depending depends document embed embedded expertsinside forget github guide helper html include included install io js list load loaded log module modules myapp needed ng-app ommitted overview package path picking plugin plugings plugins profiles reference rest search service services setup sharecoffee sharepoint src started user userprofiles utf-8 utility var warning wraps"
+      "moduleName": "ExpertsInside.SharePoint.User",
+      "shortDescription": "ExpertsInside.SharePoint.User",
+      "keywords": "$spuser api core expertsinside include js module overview profiles rest service sharepoint user userprofiles wrapper"
     },
     {
-      "section": "guide",
-      "id": "10_using-spList",
-      "shortName": "Using $spList",
-      "type": "overview",
-      "moduleName": "Using $spList",
-      "shortDescription": "Using $spList",
-      "keywords": "$log $promise $scope $splist active angular api app class completed controller created creates creating crud debug dynamically eq err error example expertsinside exposing factory filter function guide host inhostweb injecting instances interact ish items javascript js list lists loaded loading module myapp myctrl object occured operations oriented overview query querying record represent rest return select service sharepoint simple start static supported title todo todos true var view web wraps"
-    },
-    {
-      "section": "guide",
-      "id": "99_contributing",
-      "shortName": "Contributing",
-      "type": "overview",
-      "moduleName": "Contributing",
-      "shortDescription": "Contributing",
-      "keywords": "contributing dependencies dev guide installing overview"
+      "section": "api",
+      "id": "ExpertsInside.SharePoint.User.$spUser",
+      "shortName": "ExpertsInside.SharePoint.User.$spUser",
+      "type": "service",
+      "moduleName": "ExpertsInside.SharePoint.User",
+      "shortDescription": "Load user information via UserProfiles REST API",
+      "keywords": "$spconvert $sprest $spuser api core expertsinside load rest service sharepoint user userprofiles"
     }
   ],
   "apis": {
